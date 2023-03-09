e9a8b465f97b098f23921d9230c862c782de4cef
MichalIzb <michalizb@wp.pl>
2023-03-09 14:35:20 +0100

    added many components and routing handling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { FormComponent } from './form/form.component';
+import { UdemyFormComponent } from './udemy-form/udemy-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'tic-tac-toe', component: TicTacToeComponent },
+  { path: 'form-1', component: FormComponent },
+  { path: 'form-2', component: UdemyFormComponent },
+  { path: 'change-password', component: ChangePasswordComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
+import { FormComponent } from './form/form.component';
+import { UdemyFormComponent } from './udemy-form/udemy-form.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    TicTacToeComponent,
+    NotFoundComponent,
+    FormComponent,
+    UdemyFormComponent,
+    ChangePasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    NavbarComponent
+  ]
 })
 export class AppModule { }
