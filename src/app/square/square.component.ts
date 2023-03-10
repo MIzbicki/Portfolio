b7fa383a5d74312868ea4638fb76243c71907f43
MichalIzb <michalizb@wp.pl>
2023-03-10 12:28:51 +0100

    works correctly

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,14 @@
+import { X_or_O } from './../board/board.component';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.css']
+})
+
+export class SquareComponent{
+
+  @Input() value: X_or_O = X_or_O.empty;
+
+}
