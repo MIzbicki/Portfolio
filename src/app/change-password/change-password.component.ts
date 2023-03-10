d4275eff3f3c2a7a9bd9877e581e0644fe074b94
MichalIzb <michalizb@wp.pl>
2023-03-10 11:42:20 +0100

    change_password  component works correctly

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,4 +1,6 @@
+import { myValidators } from './my-password.validators';
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
@@ -12,4 +14,28 @@ export class ChangePasswordComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  form = new FormGroup({
+    oldPassword: new FormControl('', Validators.required, myValidators.validateOldPassword),
+    newPassword: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('',
+      [
+        Validators.required,
+        //myValidators.comparePasswords(this.newPassword?.value, this.confirmPassword?.value)
+      ])
+  }, myValidators.comparePasswords);
+
+  get oldPassword() {
+    return this.form.get('oldPassword');
+  }
+
+  get newPassword() {
+    return this.form.get('newPassword');
+  }
+  get confirmPassword() {
+    return this.form.get('confirmPassword');
+  }
+
+  myLog(x: any) {
+    console.log(x);
+  }
 }
